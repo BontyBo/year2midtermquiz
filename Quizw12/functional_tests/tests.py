0b2f9cad615fdbbcdf29ea7beeb9f3c26a7364e1
BontyBo <gabane1950ultrapromax@gmail.com>
2025-02-13 15:12:55 +0700

    add user story and .gitignore(pycache, chromedriver, db.sqlite3, .venv, .gitignore)

---
+++ Quizw12/functional_tests/tests.py
@@ -0,0 +1,6 @@
+#A เห็นลิงค์แสดงคำว่า Subject แล้วกดเข้าไป
+#A มีตัวเลือกแสดงคำว่า math, science, english
+#A กดตัวเลือกที่เขียนว่า math
+#A กดปุ่มที่เขียนว่า vote
+#A เห็นหน้าจอแสดง math -- 1vote, science -- 0vote, english -- 0vote
+#A เห็นลิงค์ Vote again? แล้วกด
